b6cd62ec968e844af44305fde84d75db4ee750d2
KS <kangsheng@mindvalley.com>
2021-10-17 00:57:38 +0800

    add 404

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ['./src/views/*.eta']
+    content: ['./src/views/*.eta', './src/contents/*.html']
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
